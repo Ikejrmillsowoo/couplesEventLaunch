a7a326e5982d92fe1d53de45c6dab756e285d69e
Isaac Mills-Owoo <ikejrmillsowoo@gmail.com>
2025-08-04 18:13:02 -0400

    Isolate Vite pluginn-react from backend updated

--- server/index.ts
+++ server/index.ts
@@ -56,7 +56,7 @@ app.use((req, res, next) => {
   // setting up all the other routes so the catch-all route
   // doesn't interfere with the other routes
   if (app.get("env") === "development") {
-    const { setupVite } = await import("./dev-only/vite-dev.ts");
+    const { setupVite } = await import("./dev-only/vite-dev.js");
     await setupVite(app, server);
   } else {
     const { serveStatic } = await import("./vite");
